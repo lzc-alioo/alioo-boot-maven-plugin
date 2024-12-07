6db9b52afbf4956e48f85c4b5ee23397c78ce678
悟心 <zhichong.lzc@alibaba-inc.com>
2024-12-07 10:45:56 +0800

    support lib dir

--- src/main/java/com/alioo/maven/FatJarMojo.java
+++ src/main/java/com/alioo/maven/FatJarMojo.java
@@ -32,14 +32,19 @@ public class FatJarMojo extends AbstractMojo {
             String buildDirectory = project.getBuild().getDirectory();
 
             //创建plugin.properties
+            File fatjarDir = new File(buildDirectory, "/" + project.getArtifactId());
+            if (!fatjarDir.exists()) {
+                fatjarDir.mkdirs();
+            }
 
             File classesDir = new File(buildDirectory, "/classes");
-            File libDir = new File(buildDirectory, "/lib");
+            File metaInfDir = new File(buildDirectory, "/classes/META-INF");
+            File libDir = new File(fatjarDir.getAbsolutePath(), "/lib");
+
 
             if (!libDir.exists()) {
                 libDir.mkdirs();
             }
-
             for (Object artifactObj : project.getDependencyArtifacts()) {
                 Artifact artifact = (Artifact) artifactObj;
                 if (artifact.getScope() != null && artifact.getScope().equals("provided")) {
@@ -51,53 +56,76 @@ public class FatJarMojo extends AbstractMojo {
                 FileUtils.copyFile(sourceFile, new File(libDir, sourceFile.getName()));
             }
 
-            // Prepare output jar file name
-            File fatJar = new File(buildDirectory, project.getBuild().getFinalName() + "-fat.jar");
+            //把classesDir复制到fatjarDir目录下,并且排除掉其中子目录/classes/META-INF
+            FileUtils.copyDirectory(classesDir, new File(fatjarDir.getAbsolutePath() + "/classes"), filter -> !filter.getAbsolutePath().contains("/classes/META-INF"));
+            FileUtils.copyDirectory(metaInfDir, new File(fatjarDir.getAbsolutePath() + "/META-INF"));
+
+            //将fatjarDir目录下所有文件打包到一个新的jar包中
+            File fatJar = new File(buildDirectory, project.getArtifactId() + ".jar");
             JarOutputStream jos = new JarOutputStream(new FileOutputStream(fatJar));
-
-            // 创建 classes 目录条目
-            addDir(jos, "classes/");
-            // 创建 lib 目录条目
-            addDir(jos, "lib/");
-
-
-            // Add project classes
-            addClasses(jos, classesDir, "");
-
-            for (File dependencyJar : libDir.listFiles()) {
-                addJar(jos, dependencyJar);
-            }
+            addDirectory(jos, fatjarDir);
             jos.close();
+//
+//            // 创建目录 classes/META-INF/lib
+//            mkdirDir(jos, "classes/");
+//            mkdirDir(jos, "META-INF/");
+//            mkdirDir(jos, "lib/");
+//
+//            // Add project classes
+//            addClasses(jos, classesDir);
+//            addMetaInf(jos, metaInfDir);
+//            addJar(jos, libDir);
+//
+//            jos.close();
+
+            //fatJar解压到当前目录
+
         } catch (Exception e) {
             throw new MojoExecutionException("Error assembling fat jar", e);
         }
     }
 
-    private void addClasses(JarOutputStream jos, File source, String prefix) throws IOException {
+//    private void addClasses(JarOutputStream jos, File source) throws IOException {
+//        if (source.isDirectory()) {
+//            for (File file : FileUtils.listFiles(source, null, true)) {
+//                String entryName = source.toURI().relativize(file.toURI()).getPath();
+//                if (entryName.contains("META-INF")) {
+//                    continue;
+//                }
+//                jos.putNextEntry(new JarEntry("classes/" + entryName));
+//                FileUtils.copyFile(file, jos);
+//                jos.closeEntry();
+//            }
+//        }
+//    }
+
+    private void addDirectory(JarOutputStream jos, File source) throws IOException {
         if (source.isDirectory()) {
             for (File file : FileUtils.listFiles(source, null, true)) {
-                String entryName = prefix + source.toURI().relativize(file.toURI()).getPath();
-                if (file.isFile()) {
-                    jos.putNextEntry(new JarEntry("classes/" + entryName));
-                    FileUtils.copyFile(file, jos);
-                    jos.closeEntry();
-                }
+                String entryName = source.toURI().relativize(file.toURI()).getPath();
+                jos.putNextEntry(new JarEntry(entryName));
+                FileUtils.copyFile(file, jos);
+                jos.closeEntry();
             }
         }
     }
 
-    private void addJar(JarOutputStream jos, File jarFile) throws IOException {
-        JarEntry jarEntry = new JarEntry("lib/" + jarFile.getName());
-        jos.putNextEntry(jarEntry);
-        FileUtils.copyFile(jarFile, jos);
-        jos.closeEntry();
-    }
-
-    private static void addDir(JarOutputStream jos, String dir) throws IOException {
-        JarEntry libDirEntry = new JarEntry(dir);
-        jos.putNextEntry(libDirEntry);
-        jos.closeEntry();
-    }
+//    private void addJar(JarOutputStream jos, File source) throws IOException {
+//        if (source.isDirectory()) {
+//            for (File file : FileUtils.listFiles(source, null, true)) {
+//                JarEntry jarEntry = new JarEntry("lib/" + file.getName());
+//                jos.putNextEntry(jarEntry);
+//                FileUtils.copyFile(file, jos);
+//                jos.closeEntry();
+//            }
+//        }
+//    }
+//
+//    private static void mkdirDir(JarOutputStream jos, String dir) throws IOException {
+//        JarEntry libDirEntry = new JarEntry(dir);
+//        jos.putNextEntry(libDirEntry);
+//        jos.closeEntry();
+//    }
 
     public static String toString(Build build) {
         //采用json的风格，输出入参对象的各个属性
